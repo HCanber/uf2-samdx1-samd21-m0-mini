d6a69d0e0a38d20add43e386056db5ef732ec587
Håkan Canberger <800302+HCanber@users.noreply.github.com>
2024-06-23 17:00:33 +0200

    Modify LED_PIN and remove RX/TX LED_PINs
    Based on suggestions from https://github.com/BLavery/SAMD21-M0-Mini#bootloaders-again

--- boards/samd21-m0-mini/board_config.h
+++ boards/samd21-m0-mini/board_config.h
@@ -10,8 +10,8 @@
 //#define USB_VID 0x2341
 //#define USB_PID 0x024D
 
-#define LED_PIN PIN_PA17
-#define LED_TX_PIN PIN_PA27
-#define LED_RX_PIN PIN_PB03
+#define LED_PIN PIN_PA27
+// #define LED_TX_PIN PIN_PA27
+// #define LED_RX_PIN PIN_PB03
 
 #endif
